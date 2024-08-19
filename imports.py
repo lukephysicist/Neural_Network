e2f2764d1f200bf68982e33b12b5287e9afd5607
Luke Wengler <ldwengler@gmail.com>
2024-08-19 10:50:39 -0500

    aug 19th 2024 10:50

--- imports.py
+++ imports.py
@@ -5,8 +5,14 @@ class DenseLayer:
     def __init__(self, n_inputs, n_neurons, activation_func):
         self.weights = .1 * np.random.randn(n_inputs, n_neurons)
         self.biases = np.zeros((1, n_neurons))
-        self.activation_func = activation_func
 
+        if activation_func == 'sigmoid':
+            self.activation_func = sigmoid
+
+        elif activation_func == 'softmax':
+            self.activation_func = softmax
+
+        else: self.activation_func = relu
     
     def forward(self, inputs):
         self.z = np.dot(inputs, self.weights) + self.biases
@@ -28,34 +34,53 @@ class DenseLayer:
 
 class Network:
     
-    def __init__(self, layers):
-        try:
-            assert all(isinstance(layer, DenseLayer) for layer in layers)
-        except Exception as e:
-            print("must initialize Network with list of Layer objects")
+    def __init__(self, layers, loss_func = 'mse'):
+        if not all(isinstance(layer, DenseLayer) for layer in layers):
+            raise NetworkErrorOne("""must initialize Network with list of Layer objects.""")
+        
+        
+        for i in range(len(layers) - 1):
+            if layers[i].weights.shape[1] != layers[i+1].weights.shape[0]:
+                raise NetworkErrorTwo("Each layer must have n_inputs equal to n_neurons in previous layer")
+                
+        self.layers = layers
 
-        try:
-            for i in range(len(layers) - 1):
-                assert layers[i].weights.shape[1] == layers[i+1].weights.shape[0]
-        except:
-            print("number of inputs for one layer must be the same as the number of neurons for previous layer")
-
-        self.layers = [layer for layer in layers]
+        if loss_func == 'cat_cross_entropy':
+            self.loss_func = cat_cross_entropy
+        
+        else: self.loss_func = mean_sq_error
 
 
-    def network_forward(self, network_inputs):
-        try:
-            assert network_inputs.shape[1] == self.layers[0].shape[0]
-        except:
-            print(f"network has {self.layers[0].shape[0]} dimensional inputs, you gave {network_inputs.shape[1]}")
+    def network_forward(self, network_inputs, truths = np.array([])):
+        if network_inputs.shape[0] != self.layers[0].weights.shape[0]:
+            raise NetworkErrorThree(f'''
+                the network has {self.layers[0].weights.shape[0]} dimensional inputs.
+                You gave a{network_inputs.shape[0]} dimensional input''')
 
         layer_inputs = network_inputs
         for layer in self.layers:
             layer_inputs = layer.forward(layer_inputs)
 
-        return layer_inputs
+        self.out = layer_inputs
+
+        if len(truths) != 0:
+            self.loss = self.loss_func(self.out, truths)
+
+        return self.out
 
 
+
+class NetworkErrorOne(Exception):
+    """For if the network object is not passed a list"""
+    
+
+
+class NetworkErrorTwo(Exception):
+    """for if adjacent layers dont fit together"""
+
+
+class NetworkErrorThree(Exception):
+    """for when an incorrect number of inputs is provided to the network"""
 
 def relu(inputs):
     return np.maximum(0, inputs)
