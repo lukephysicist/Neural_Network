8be2f03cbea42130671b69499343f81aeefb8502
Luke Wengler <ldwengler@gmail.com>
2024-08-30 11:15:49 -0500

    working epoch reshuffle

--- imports.py
+++ imports.py
@@ -1,7 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+import random
 
-class DenseLayer:
+class Layer:
     
     def __init__(self, n_inputs, n_neurons, activation_func):
 
@@ -65,11 +67,10 @@ class DenseLayer:
         
 
 
-
 class Network:
     
-    def __init__(self, layers, loss_func = 'mse', rate = .01, regressor = True):
-        if not all(isinstance(layer, DenseLayer) for layer in layers):
+    def __init__(self, layers, rate = .01, regressor = True):
+        if not all(isinstance(layer, Layer) for layer in layers):
             raise NetworkErrorOne("""must initialize Network with list of Layer objects.""")
         
         
@@ -84,27 +85,23 @@ class Network:
         self.rate = rate
         self.loss = None
         self.regressor = regressor
-
-        if loss_func == 'cat_cross_entropy':
-            self.loss_func = cat_cross_entropy
-        
-        else: self.loss_func = mean_sq_error
+        self.loss_func = mean_sq_error if self.regressor else cat_cross_entropy
 
 
     def network_forward(self, network_inputs, truths = np.array([])):
+
         if network_inputs.shape[1] != self.layers[0].weights.shape[0]:
             raise NetworkErrorThree(f'''
                 the network has {self.layers[0].weights.shape[0]} dimensional inputs.
-                You gave a {network_inputs.shape[1]} dimensional input''')
+                You gave a {network_inputs.shape[1]} dimensional input'''
+                )
 
         layer_inputs = network_inputs
         for layer in self.layers:
             layer_inputs = layer.forward(layer_inputs)
 
         self.out = layer_inputs
-
-        if len(truths) != 0:
-            self.loss = self.loss_func(self.out, truths)
+        self.loss = self.loss_func(self.out, truths)
 
         return self.out
 
@@ -114,20 +111,29 @@ class Network:
 
     def train_test(self, training_samples, testing_samples, batch_size, n_epochs):
         
-        # batches the training samples
-        training_set = [dict(list(training_samples.items())[i: i+batch_size]) for i in range(0, len(training_samples.items()), batch_size)]
+        def new_epoch(traning_samples, batch_size):
+            # shuffles the traning data 
+            random.shuffle(list(training_samples.items()))
+            traning_samples = list(traning_samples)
 
-        # creates a list of traning input batches and their answers
-        network_inputs = [np.array(list(batch.keys())) for batch in training_set]
-        network_truths = [np.array(list(batch.values())).reshape(-1,1) for batch in training_set]
+            training_set = [dict(list(training_samples.items())[i: i+batch_size]) for i in range(0, len(training_samples.items()), batch_size)]
 
+            network_inputs = [np.array(list(batch.keys())) for batch in training_set]
+            network_truths = [np.array(list(batch.values())).reshape(-1,1) for batch in training_set]
+
+            return (network_inputs, network_truths)
+        
         # trains the network on each batch in the traning set
         loss_ys = []
         for j in range(n_epochs):
+            # shuffle the batches for every epoch
+            network_inputs, network_truths = new_epoch(training_samples, batch_size)
             for i in range(len(network_inputs)):
                 self.network_forward(network_inputs=network_inputs[i], truths=network_truths[i])
                 loss_ys.append(self.loss)
                 self.network_back(network_truths[i])
+            
+
 
         # creates a list of testing inputs batches and their answers
         testing_inputs = np.array(list(testing_samples.keys()))
@@ -137,17 +143,23 @@ class Network:
         loss_final = self.loss
         loss_ys.append(loss_final)
 
-        plt.plot(loss_ys)
-        plt.xlabel('Batch')
-        plt.ylabel('Loss')
-        plt.title('Loss After Backprop')
-        plt.show()    
-        
-        if self.regressor:    
-            print(f'MSE: {loss_final}')
-        
-        print(f'test set preds: {testing_preds}')
-        print(f'testing truths: {testing_truths}')
+        if self.regressor:
+            # plot loss after each forward pass
+            plt.plot(loss_ys)
+            plt.xlabel('Batch')
+            plt.ylabel('Mean Sq Error')
+            plt.title('Loss')
+            plt.show()
+            print(f'MSE: {loss_final}\n')
+
+            df = pd.DataFrame(
+                {
+                "Model Predictions" : [pred[0] for pred in testing_preds],
+                "Truths" : [truth[0] for truth in testing_truths]
+                }
+            )
+            return df
+
 
     
 
